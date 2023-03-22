b02aa77714e7aa7322b77f316db24e03b00c869f
Desc <63260058+descrysama@users.noreply.github.com>
2023-03-22 20:04:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 let todoListArray = [];
 let todoInput = document.getElementById('todo-input');
-
 function createButton(todo, index) {
     let div = document.createElement('div');
     let li = document.createElement('li');
     let button = document.createElement('button');
-
     li.innerHTML = todo.content;
     li.id = index;
     li.classList.add = "single-todo";
@@ -20,7 +18,6 @@ function createButton(todo, index) {
         todoListArray.splice(index, 1, newObject);
         loadToDoList()
     })
-
     button.innerHTML = 'X';
     button.id = index;
     button.style.marginLeft = '5px'
@@ -28,27 +25,21 @@ function createButton(todo, index) {
         todoListArray.splice(index, 1)
         loadToDoList()
     })
-
     div.appendChild(li);
     div.appendChild(button);
-
     div.style.display = 'flex';
     div.style.alignItems = 'center';
     button.style.margin = '5px'
-
     return div;
 }
-
 function loadToDoList() {
     document.getElementById('todo-list').innerHTML = "";
     for(let i = 0; i < todoListArray.length; i++) {
         document.getElementById('todo-list').appendChild(createButton(todoListArray[i], i));
     }
 }
-
 document.getElementById('form').addEventListener('submit', (event) => {
     event.preventDefault();
-
     if(todoInput.value) {
         todoListArray.push({
             content: todoInput.value,
@@ -57,7 +48,5 @@ document.getElementById('form').addEventListener('submit', (event) => {
         document.getElementById('todo-input').value = "";
         loadToDoList()
     }
-    
 })
-
 loadToDoList();
