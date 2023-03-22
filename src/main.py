2072957fb478e9c6be05453457b87b5a0cefb9bc
Desc <63260058+descrysama@users.noreply.github.com>
2023-03-22 19:58:20 +0100

    Delete src directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-
-try:
-    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-    # Add your code here to navigate to a website or perform some action
-except Exception as e:
-    print(e)
